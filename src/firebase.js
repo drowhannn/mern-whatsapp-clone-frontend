c64e2dbc4b20fa98b036a6d211065d8cf1805ac0
Rohan Dhimal <drowhann@gmail.com>
2021-09-13 19:31:24 +0545

    just created react app

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDL9nK9OOHlGUs4n5oBa1n5uDZv4l4E8UE",
+  authDomain: "mern-whatsapp-clone-e4535.firebaseapp.com",
+  projectId: "mern-whatsapp-clone-e4535",
+  storageBucket: "mern-whatsapp-clone-e4535.appspot.com",
+  messagingSenderId: "993267268028",
+  appId: "1:993267268028:web:e8791a9e1172b7619d93b9",
+};
